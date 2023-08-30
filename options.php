d2b04b67a12059afbaf5be6e6a43d1565c4ee67a
AlexAce13 <oleksandr.klishch2003@gmail.com>
2023-08-30 11:22:39 +0300

    secure strings

--- options.php
+++ options.php
@@ -20,29 +20,29 @@ function register_my_breadcrumbs_settings() {
 
 function my_breadcrumbs_settings_page() { ?>
     <div class="wrap">
-        <h1>WP breadcrumbs</h1>
+        <h1><?php esc_attr('WP breadcrumbs') ?></h1>
         <form method="post" action="options.php">
 			<?php settings_fields( 'my-breadcrumbs-settings-group' ); ?>
 			<?php do_settings_sections( 'my-breadcrumbs-settings-group' ); ?>
             <table class="form-table">
                 <tr>
-                    <th scope="row">Show current</th>
+                    <th scope="row"><?php esc_attr('Show current') ?></th>
                     <td><input type="checkbox" name="show_current" value="1" <?php checked( 1, get_option('show_current') ) ?> /></td>
                 </tr>
                 <tr>
-                    <th scope="row">Show last separator</th>
+                    <th scope="row"><?php esc_attr('Show last separator') ?></th>
                     <td><input type="checkbox" name="show_last_sep" value="1" <?php checked( 1, get_option('show_last_sep') ) ?>/></td>
                 </tr>
                 <tr>
-                    <th scope="row">Container class</th>
+                    <th scope="row"><?php esc_attr('Container class') ?></th>
                     <td><input type="text" name="container_class" value="<?php echo esc_attr( get_option('container_class') ); ?>" /></td>
                 </tr>
                 <tr>
-                    <th scope="row">Breadcrumbs list class</th>
+                    <th scope="row"><?php esc_attr('Breadcrumbs list class') ?></th>
                     <td><input type="text" name="breadcrumbs_list_class" value="<?php echo esc_attr( get_option('breadcrumbs_list_class') ); ?>" /></td>
                 </tr>
                 <tr>
-                    <th scope="row">Breadcrumbs item class</th>
+                    <th scope="row"><?php esc_attr('Breadcrumbs item class') ?></th>
                     <td><input type="text" name="breadcrumbs_item_class" value="<?php echo esc_attr( get_option('breadcrumbs_item_class') ); ?>" /></td>
                 </tr>
             </table>
